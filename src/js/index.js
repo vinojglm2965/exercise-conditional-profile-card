7795834ece2d47bdf1ae9451e286f7cf20a5d8cb
José Lorant <lorantmarquina@gmail.com>
2020-08-04 05:50:03 +0000

    cambios

--- src/js/index.js
+++ src/js/index.js
@@ -33,14 +33,14 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
+          <h1>Jose Lorant</h1>
           <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h3>SANTIAGO, CHILE</h3>
           <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/joselorant2"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://github.com/vinojglm2965"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/jos%C3%A9-gabriel-lorant-morales-86b5ba1a6/"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://www.instagram.com/jglorant/?hl=es-la"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
